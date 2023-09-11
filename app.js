f357816333a601d31da7137fd1f1efe26fdee482
Ayomide <@vayomide2@gmail.com>
2023-09-11 03:01:37 +0100

    changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+import express from 'express'
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/endpoint', (req, res) => {
+
+app.get('/api', (req, res) => {
   const midewears02 = req.query.midewears02;
-  const track = req.query.Backend;
+  const track = req.query.backend;
 
   // Get the current day of the week in full format
   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -15,8 +15,8 @@ app.get('/endpoint', (req, res) => {
   const utcTime = new Date(now.getTime() + (now.getTimezoneOffset() - 120) * 60000).toISOString();
 
   // Replace with your GitHub repository and file name
-  const githubRepoURL = "https://github.com/Midesales/ZuriIntern-Backend-stageone";
-  const githubFileURL = `${githubRepoURL}/blob/master/app.js`;
+  const githubRepoURL = "https://github.com/Midesales/Zuri-Backend/tree/master/stage-one";
+  const githubFileURL = `https://github.com/Midesales/Zuri-Backend/blob/master/stage-one/app.js`;
 
   // Create the response JSON object
   const responseObject = {
@@ -31,6 +31,10 @@ app.get('/endpoint', (req, res) => {
 
   res.json(responseObject);
 });
+
+const port = 3000;
+
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
